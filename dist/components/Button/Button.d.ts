1caad9de71b57146bc4b1426f3be13de56cfde17
ruslanonly <rzaevruslanonly@gmail.com>
2023-01-14 15:35:15 +0300

    Create basis for compatible UI development   - configure basic webpack   - create ./examples folder for testing   - configure package.json, gitignore and npmignore.

---
+++ dist/components/Button/Button.d.ts
@@ -0,0 +1,6 @@
+import { FC, PropsWithChildren } from 'react';
+interface IButtonProps extends PropsWithChildren {
+    className?: string;
+}
+declare const Button: FC<IButtonProps>;
+export default Button;
